a14696e9d606cfc927f7f95a8b18ef6519deed90
lungcollapser <milesterrell1337@gmail.com>
2024-09-19 07:55:16 -0500

    walking talking bean
    added mouse camera movement and character movement

--- scenes/character_body_3d.gd
+++ scenes/character_body_3d.gd
@@ -3,6 +3,20 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const SENSITIVTY = 0.03
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVTY)
+		camera.rotate_x(-event.relative.y * SENSITIVTY)
+		
 
 
 func _physics_process(delta: float) -> void:
@@ -16,13 +30,13 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = 0.0
+		velocity.z = 0.0
 
 	move_and_slide()
