cb29d6d91fba97b770aa6c572530133ced5820d6
lungcollapser <milesterrell1337@gmail.com>
2024-09-19 08:10:41 -0500

    working on headbob functionality

--- scenes/character_body_3d.gd
+++ scenes/character_body_3d.gd
@@ -3,7 +3,12 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const SENSITIVTY = 0.03
+const SENSITIVTY = 0.02
+
+# Headbob variables
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
@@ -16,6 +21,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVTY)
 		camera.rotate_x(-event.relative.y * SENSITIVTY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 		
 
 
@@ -31,7 +37,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -39,4 +45,17 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0.0
 		velocity.z = 0.0
 
+	
+# Head Bob
+
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform_origin = headbob(t_bob)
+	
 	move_and_slide()
+	
+	
+func headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	return pos
+	
