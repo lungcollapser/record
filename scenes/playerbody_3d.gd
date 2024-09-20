f0aabfb37d8010d5a03d0defc944d77e2661f654
lungcollapser <milesterrell1337@gmail.com>
2024-09-19 21:01:14 -0500

    sprint and walk functionality

--- scenes/playerbody_3d.gd
+++ scenes/playerbody_3d.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+var speed = 4.5
+const WALK_SPEED = 4.5
+const SPRINT_SPEED = 30.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVTY = 0.02
 
@@ -21,7 +22,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVTY)
 		camera.rotate_x(-event.relative.y * SENSITIVTY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-50), deg_to_rad(60))
 		
 
 
@@ -29,6 +30,11 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = speed
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -39,8 +45,8 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
