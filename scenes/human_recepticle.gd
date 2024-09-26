2e23c9817c206918b7e51cb2fdeac28dca6b9c06
lungcollapser <milesterrell1337@gmail.com>
2024-09-26 00:01:40 -0500

    queue free

--- scenes/human_recepticle.gd
+++ scenes/human_recepticle.gd
@@ -3,4 +3,3 @@ extends RigidBody3D
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int):
 	if body.is_in_group("Bussin"):
 		body.queue_free()
-	print("test")
--- scripts/dead_body.gd
+++ scripts/dead_body.gd
@@ -1,7 +1,1 @@
 extends RigidBody3D
-
-
-@onready var collision_shape = $CollisionShape3D
-
-func grind_body(body):
-	queue_free()
