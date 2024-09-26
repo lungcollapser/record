26800396d12954bf90ad0c0dfcf4212bb8d8bda7
lungcollapser <milesterrell1337@gmail.com>
2024-09-25 23:37:57 -0500

    nathan is smarter than any of you monkeys

--- dead_body.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody3D
-
-@onready var dead_body = $deadbody
-@onready var collision_shape = $CollisionShape3D
-@onready var ray = $Node3D/RayCast3D
-
-func grind_body():
-	queue_free()
