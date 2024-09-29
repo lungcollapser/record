829e58abad0afd22de76b84d3d580b7b6a20b5cd
lungcollapser <milesterrell1337@gmail.com>
2024-09-29 18:06:39 -0500

    drop and pick up :D

--- scripts/human_recepticle.gd
+++ scripts/human_recepticle.gd
@@ -1,5 +1,7 @@
 extends RigidBody3D
 
+@onready var collision = $HumanReceptacleShape
+
 func _on_body_shape_entered(_body_rid: RID, body: Node, _body_shape_index: int, _local_shape_index: int):
 	if body.is_in_group("Bussin"):
 		body.queue_free()
@@ -7,5 +9,10 @@ func _on_body_shape_entered(_body_rid: RID, body: Node, _body_shape_index: int, 
 
 func stow_receptacle():
 	if Input.is_action_just_pressed("stow"):
-		queue_free()
-		
+		collision.disabled = true
+		visible = false
+	if Input.is_action_just_pressed("dropreceptacle"):
+		collision.disabled = false
+		visible = true
+func _process(delta: float) -> void:
+	stow_receptacle()
