ead3196f5afcf0331c81c2e61bab33da9896883a
lungcollapser <milesterrell1337@gmail.com>
2024-10-02 21:37:42 -0500

    i peed

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,18 +2,25 @@ extends CharacterBody3D
 
 const enemy_speed = 3
 var enemy_health = 100
-var player = null
+var player
 @export var player_path : NodePath
 @onready var nav_agent = $NavigationAgent3D
 
 func _ready():
 	player = get_node(player_path)
 	
-func _process(delta: float):
-	velocity = Vector3.ZERO
-	nav_agent.set_target_position(player.global_position)
-	var next_nav_point = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - global_position).normalized() * enemy_speed
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 	
-	move_and_slide()
+	nav_agent.target_position = player.global_position
+	
+	var next_location = nav_agent.get_next_path_position()
+	
+	if not nav_agent.is_target_reached():
+		var direction = (next_location - global_position)
+		direction.y = 0
+		
+		move_and_collide(direction * 1)
+
 	
