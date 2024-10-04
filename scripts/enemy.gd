c64e4380fb9ed0b7423ed4a5f59dd64d985d0bcd
lungcollapser <milesterrell1337@gmail.com>
2024-10-03 22:39:52 -0500

    trying to add sum detection

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,23 +3,23 @@ extends CharacterBody3D
 const ENEMY_SPEED = 0.1
 var enemy_health = 100
 var player
+var player_area
 @export var player_path : NodePath
-@onready var nav_agent = $NavigationAgent3D
+@onready var nav_agent = $EnemyNavigation
 
 func _ready():
-	call_deferred("enemy_setup")
+	call_deferred("enemy_chase")
 	player = get_tree().get_nodes_in_group("player")[0]
+	player_area = get_tree().get_nodes_in_group("player_area")
 	
-	
-	
-func enemy_setup():
-	await get_tree().physics_frame
+func enemy_chase():
 	nav_agent.set_target_position(player.global_position)
 	var velocity = (nav_agent.get_next_path_position() - global_position).normalized() * ENEMY_SPEED
 	move_and_collide(velocity)
 	
-	
-func _physics_process(_delta: float):
-	enemy_setup()
-	
+
+func _on_area_3d_body_entered(body):
+	if body == player:
+		enemy_chase()
+		print("poopy")
 	
