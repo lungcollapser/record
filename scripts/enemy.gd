90517fa65dbac2d0ee15f6c7a5c315339c6e1788
lungcollapser <milesterrell1337@gmail.com>
2024-10-06 10:25:55 -0500

    trying to get da roamin

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,21 +1,29 @@
 extends CharacterBody3D
 
-const ENEMY_SPEED = 0.02
+const ENEMY_SPEED = 0.1
 var enemy_health = 100
 var player
 var player_area
 var target
+var enemy_navigation_link
 @export var player_path : NodePath
 @onready var nav_agent = $EnemyNavigation
 @onready var enemy_area = $EnemyArea
 
+
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
-	player_area = get_tree().get_nodes_in_group("player_area")
+	enemy_navigation_link = get_tree().get_nodes_in_group("Roamin")[0]
 	
 func _physics_process(_delta: float) -> void:
 	enemy_chase()
+	enemy_roaming()
 
+func enemy_roaming():
+	if target == null:
+		await get_tree().physics_frame
+		var roaming_velocity = enemy_navigation_link.end_position * ENEMY_SPEED
+		move_and_collide(roaming_velocity)
 	
 func enemy_chase():
 	if target == Player:
