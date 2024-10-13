1ca9597b6ecf2478e5c9b7e2ce18f838918a2335
lungcollapser <milesterrell1337@gmail.com>
2024-10-12 21:38:04 -0500

    fixed bodyparts and receptacle

--- scripts/dead_body.gd
+++ scripts/dead_body.gd
@@ -1,15 +1,24 @@
 extends RigidBody3D
 
-var pick_up
+var player_hold
+var player_pick_up
 @onready var dead_body_shape = $deadbodyshape
+@onready var dead_body_parts = preload("res://scenes/dead_body_parts.tscn")
 
 func _ready() -> void:
-	pick_up = get_tree().get_nodes_in_group("pickup")[0]
-	
+	player_hold = get_tree().get_nodes_in_group("hold")[0]
+	player_pick_up = get_tree().get_nodes_in_group("pickup")[0]
 
 
 func _physics_process(_delta: float) -> void:
-	var dismember_collider = pick_up.get_collider()
+	var dismember_collider = player_pick_up.get_collider()
+	var dead_body_parts_instance = dead_body_parts.instantiate()
 	if Input.is_action_just_pressed("attack") and dismember_collider != null and dismember_collider.is_in_group("Dismember"):
 		visible = false
 		dead_body_shape.disabled = true
+
+	
+	if Input.is_action_just_pressed("attack") and dismember_collider != null and dismember_collider.is_in_group("Dismember"):
+		get_parent().add_child(dead_body_parts_instance)
+		dead_body_parts_instance.global_position = player_hold.global_position 
+	
